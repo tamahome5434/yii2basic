c67fc0eea6e66ad6265fd4b327d5a6ca0337de99
tamahome5434 <tamahome5434@hotmail.com>
2016-10-24 00:50:14 +0800

    Update calculation

--- web/js/checkout.js
+++ web/js/checkout.js
@@ -1,80 +1,64 @@
 $(document).ready(function() {
+	
 	var qty=$('#qty').val();
 	var subtotal=$('#subtotal').val();	
-	$('#discount').val(0);
+	$('#discount').html('0');
 	$("#apply").click(function()  {  
 		var code=$('#code').val().toUpperCase();
-		var off5=(subtotal*0.05).toFixed(2);
-		var off15=subtotal-15;
 	
 		if(code=='OFF5PC'){
 			if (qty>=2)
-				$('#discount').val(off5);
+				$('#discount').html((subtotal*0.05).toFixed(2));
 			else{
-				$('#discount').val(0);
 				alert('At least 2 quantities');
+				$('#discount').html('0');
+				$('#code').val('');
 			}
 		}
 		else if(code=='GIVEME15'){
 			if(subtotal>=100)
-				$('#discount').val(off15);
+				$('#discount').html('15');
 			else{
-				$('#discount').val(0);
 				alert('Minumum puchase of RM100');
+				$('#discount').html('0');
+				$('#code').val('');
 			}
 		}
-		else{
-			$('#discount').val(0);
+		else{	
+			$('#discount').html('0');
 			alert('Invalid promotion code');
-		}
-		if ($('#discount').val()=='0'){
+			$('#discount').html('0');
 			$('#code').val('');
 		}
-			
 		
 	});
 
 	
-	if(qty>=2||subtotal>=150){
-		$('#ship').val(0);
+	if(qty>=2||subtotal>=150)
 		$('#shipping').html('0');
-	}
-	else{
-		$('#ship').val(10);
+	else
 		$('#shipping').html('10');
-	}
+	
 	$("#country").change(function() {
 		var country=$('#country').val();
 		
 		if(country=='malaysia'){
-			if(qty>=2||subtotal>=150){
-				$('#ship').val(0);
+			if(qty>=2||subtotal>=150)
 				$('#shipping').html('0');
-			}
-			else{
-				$('#ship').val(10);
+			else
 				$('#shipping').html('10');
-			}
 		}
 		else if(country=='singapore'){
-			if(subtotal>=300){
-				$('#ship').val(0);
+			if(subtotal>=300)
 				$('#shipping').html('0');
-			}
-			else{
-				$('#ship').val(20);
+			else
 				$('#shipping').html('20');
-			}
 		}
 		else if(country=='brunei')	{
-			if(subtotal>=300){
-				$('#ship').val(0);
+			if(subtotal>=300)
 				$('#shipping').html('0');
-			}
-			else{
-				$('#ship').val(25);
+			else
 				$('#shipping').html('25');
-			}
 		}
-	});
+	}); 
 });
